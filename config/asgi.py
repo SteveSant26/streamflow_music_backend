6b4f257e0ee151ceed6e6fbbcf285d934047c297
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-27 22:32:52 -0500

    .

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
 
 application = get_asgi_application()
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
 
 application = get_wsgi_application()
