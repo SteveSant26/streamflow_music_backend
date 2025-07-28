da7e768a07c17b9b13d1b167b8a954342d4926c2
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-27 22:43:09 -0500

    fix: asgi y wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 import sys
 
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR, "src"))
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 
 application = get_wsgi_application()
