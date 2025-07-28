97b3845831858e6700712d21501acdd14b6527c6
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-27 21:43:25 -0500

    fix: add ignore for autoflake

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,6 +2,18 @@ import os
 
 from src.common.utils import LoggingConfig
 
+from .apps_settings import INSTALLED_APPS, SWAGGER_SETTINGS  # noqa: F401
+from .auth_settings import AUTH_PASSWORD_VALIDATORS  # noqa: F401
+from .database_settings import DATABASES  # noqa: F401
+from .jazzmin_settings import JAZZMIN_SETTINGS, JAZZMIN_UI_TWEAKS  # noqa: F401
+from .middleware_settings import MIDDLEWARE  # noqa: F401
+from .rest_framework_settings import REST_FRAMEWORK  # noqa: F401
+from .supabase_settings import SUPABASE_ANON_KEY  # noqa: F401
+from .supabase_settings import SUPABASE_JWT_ALGORITHM  # noqa: F401
+from .supabase_settings import SUPABASE_JWT_SECRET  # noqa: F401
+from .supabase_settings import SUPABASE_PROJECT_ID  # noqa: F401
+from .supabase_settings import SUPABASE_SERVICE_KEY  # noqa: F401
+from .templates_settings import TEMPLATES  # noqa: F401
 from .utils.env import BASE_DIR, ENVIRONMENT, env
 
 SECRET_KEY = env("SECRET_KEY")
