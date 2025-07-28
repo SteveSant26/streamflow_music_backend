42db7eda615bef69a83a427bf2311691483f7257
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-27 20:24:41 -0500

    fix: apply good coding practices

--- config/settings/rest_framework_settings.py
+++ config/settings/rest_framework_settings.py
@@ -8,6 +8,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "common.core.pagination.CustomPagination",
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
--- src/apps/user_profile/infrastructure/repository/user_repository.py
+++ src/apps/user_profile/infrastructure/repository/user_repository.py
@@ -1,40 +1,51 @@
+from apps.user_profile.domain.repository.Iuser_repository import IUserRepository
+
 from ...domain.entities import UserEntity
 from ...infrastructure.models import UserProfile
 
 
-class UserRepository:
+class UserRepository(IUserRepository):
     def __init__(self):
         self.model = UserProfile
 
     def get_user_by_id(self, user_id: str) -> UserEntity | None:
         try:
             user = self.model.objects.get(pk=user_id)
-            return self._to_entity(user)
+            return self._model_to_entity(user)
         except self.model.DoesNotExist:
             return None
 
     def get_by_email(self, email: str) -> UserEntity | None:
         try:
             user = self.model.objects.get(email=email)
-            return self._to_entity(user)
+            return self._model_to_entity(user)
         except self.model.DoesNotExist:
             return None
 
-    def save(self, user_entity: UserEntity) -> UserEntity:
-        print()
-        print()
-        print()
-        print(user_entity.id)
-
+    def save(self, entity: UserEntity) -> UserEntity:
         user, _ = self.model.objects.update_or_create(
-            id=user_entity.id,
-            email=user_entity.email,
+            id=entity.id,
+            email=entity.email,
+        )
+        return self._model_to_entity(user)
+
+    def delete(self, entity_id: str) -> None:
+        self.model.objects.filter(pk=entity_id).delete()
+
+    def update(self, entity_id: str, entity: UserEntity) -> UserEntity:
+        user = self.model.objects.get(pk=entity_id)
+        user.email = entity.email
+        user.save()
+        return self._model_to_entity(user)
+
+    def _entity_to_model(self, entity: UserEntity) -> UserProfile:
+        return UserProfile(
+            id=entity.id,
+            email=entity.email,
         )
 
-        return self._to_entity(user)
-
-    def _to_entity(self, user: UserProfile) -> UserEntity:
+    def _model_to_entity(self, model: UserProfile) -> UserEntity:
         return UserEntity(
-            id=str(user.id),
-            email=user.email,
+            id=str(model.id),
+            email=model.email,
         )
