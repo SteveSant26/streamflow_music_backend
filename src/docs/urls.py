2780619fa48340e330c9da22663f0f2188ca5aef
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-27 22:50:07 -0500

    fix: asgi y wsgi

--- src/docs/urls.py
+++ src/docs/urls.py
@@ -1,15 +1,21 @@
+from django.conf import settings
 from django.urls import path
 
 from .schema import schema_view
 
-urlpatterns = [
-    path(
-        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+if settings.DEBUG:
+    urlpatterns = [
+        path(
+            "swagger<format>/",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        path(
+            "swagger/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        ),
+    ]
