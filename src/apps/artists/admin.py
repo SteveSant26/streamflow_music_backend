c3a72f99a28edf5eb2c2b21adb7fd872c1146c65
Arteaag Molina Cesar Emanuel <cesarjunior30xd@gmail.com>
2025-08-03 18:19:21 -0500

    Update admin.py

--- src/apps/artists/admin.py
+++ src/apps/artists/admin.py
@@ -1,7 +1,25 @@
-# from .infrastructure.models import ArtistModel
-# admin.site.register(ArtistModel)
-
 from django.contrib import admin
 from .infrastructure.models import ArtistModel
 
-admin.site.register(ArtistModel)
+
+@admin.register(ArtistModel)
+class ArtistModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "country",
+        "followers_count",
+        "is_verified",
+        "is_active",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "country",
+    )
+    list_filter = (
+        "is_verified",
+        "is_active",
+        "country",
+    )
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at", "followers_count")
