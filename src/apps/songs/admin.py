99d2c4d213ca5ec32113ba6e8c208516f4bf12c1
Arteaag Molina Cesar Emanuel <cesarjunior30xd@gmail.com>
2025-08-03 18:12:30 -0500

    Update admin.py
    registro de modelos mas mejor

--- src/apps/songs/admin.py
+++ src/apps/songs/admin.py
@@ -1,7 +1,36 @@
-# from .infrastructure.models import SongModel
-# admin.site.register(SongModel)
-
 from django.contrib import admin
 from .infrastructure.models import SongModel
 
-admin.site.register(SongModel)
+@admin.register(SongModel)
+class SongModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "artist_name",
+        "album_title",
+        "genre_name",
+        "duration_formatted",
+        "play_count",
+        "favorite_count",
+        "is_active",
+        "is_premium",
+        "audio_quality",
+        "release_date",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "artist_name",
+        "album_title",
+        "genre_name",
+        "source_id",
+    )
+    list_filter = (
+        "is_active",
+        "is_premium",
+        "is_explicit",
+        "audio_quality",
+        "source_type",
+        "created_at",
+    )
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at", "last_played_at", "play_count", "favorite_count", "download_count")
