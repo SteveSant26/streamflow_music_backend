7337020d17eb5566f2537e9bdfcf3fd91fdbe88a
lesquel <lesquel1319@gmail.com>
2025-08-04 18:02:00 -0500

    refactor: simplify song retrieval in SongDetailView by removing async wrapper

--- src/apps/songs/api/views/song_detail_view.py
+++ src/apps/songs/api/views/song_detail_view.py
@@ -1,4 +1,3 @@
-from asgiref.sync import async_to_sync
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import status
 from rest_framework.response import Response
@@ -29,10 +28,7 @@ class SongDetailView(UseCaseAPIViewMixin):
     def get(self, request, song_id):
         """Obtiene detalles de una canción"""
 
-        def execute_use_case():
-            return async_to_sync(self.get_song_by_id_use_case.execute)(song_id)
-
-        song = self.handle_use_case_execution(execute_use_case)
+        song = self.handle_use_case_execution(self.get_song_by_id_use_case, song_id)
         song_dto = self.mapper.entity_to_dto(song)
         serializer = SongSerializer(song_dto)
 
